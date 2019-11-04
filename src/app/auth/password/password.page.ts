f586210db12929135c66c8ef3fe513e17a1c461e
David Lerner <lernerlombrozo@gmail.com>
2019-11-04 09:48:57 -0700

    added firebase

--- src/app/auth/password/password.page.ts
+++ src/app/auth/password/password.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-password',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordPage implements OnInit {
 
-  constructor() { }
+  email:string;
+
+  constructor(
+    public authService:AuthService,
+    public alertCtrl:AlertController,
+    public loadingCtrl:LoadingController,
+    ) { }
 
   ngOnInit() {
+  }
+
+  async password(){
+    var loading = await  this.loadingCtrl.create(
+      {
+        message: 'Signing In...',
+        spinner: 'crescent',
+      }
+    )
+    loading.present()
+    this.authService.recoverPassword(this.email)
+      .then((success)=>{
+        loading.dismiss()
+        this.presentAlert('success',JSON.stringify(success))
+      })
+      .catch((err)=>{
+        loading.dismiss()
+        this.presentAlert('error',JSON.stringify(err))
+      })
+  }
+
+  async presentAlert(title, message) {
+    const alert = await this.alertCtrl.create({
+      header: title,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
 }
